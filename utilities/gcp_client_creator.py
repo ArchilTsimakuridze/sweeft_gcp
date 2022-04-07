2565d6b0a9cd4df3e33f79d2e7d196b93ce95d3f
Archil Tsimakuridze <a.tsimakuridze16@gmail.com>
2022-04-07 14:46:14 +0400

    Updating file with docstrings

--- utilities/gcp_client_creator.py
+++ utilities/gcp_client_creator.py
@@ -9,20 +9,24 @@ from helpers.json_formatter import json_to_dict
 
 @dataclass
 class CredentialManager:
+    """Returns a client from credentials.json and project name"""
 
     path: str
     project_name: Optional[str] = None
 
     @property
     def credentials_dict(self):
+        """Transforms json into dict"""
         return json_to_dict(self.path)
 
     @property
     def get_credentials(self):
+        """Returns credentials from a credentials dict"""
         return ServiceAccountCredentials.from_json_keyfile_dict(
             keyfile_dict=self.credentials_dict)
 
     @property
     def get_client(self):
+        """Returns a client for a given credentials and a project name"""
         return storage.Client(credentials=self.get_credentials,
                               project=self.project_name)
