b533626b0ee205ecb4970dbb2c30048a3b497cf6
Archil Tsimakuidze <a.tsimakuridze16@gmail.com>
2022-04-09 12:29:04 +0400

    Updating blob manager with docstrings and removing testing lines

--- blob_manager.py
+++ blob_manager.py
@@ -2,15 +2,19 @@ from utilities.gcp_blob_creator import BlobCreator
 from helpers.path_formatter import absolute_path
 
 
-BUCKET_NAME = 'maridashvili-bucket'
-BLOB_NAME = 'archil.csv'
-BLOB_PATH = 'some_folder'
-PROJECT_NAME = 'crud-project'
-CREDENTIALS_JSON = absolute_path('temporary_config/credentials.json')
-
-
 class BlobManager:
+    """Creates a blob object from BlobCreator class and applies CRUD
+    functionality to it
+    Args:
+    credentials_path: Path to service account credentials file: Str
+    project_name: Str
+    blob_name: Str
+    blob_path: Str
+    bucket_name: Str
 
+    Class methods upload/update, delete and download a given blob
+    or a file
+    """
     def __init__(self, credentials_path, project_name, bucket_name,
                  blob_name, blob_path):
         self.credentials_path = credentials_path
@@ -28,25 +32,23 @@ class BlobManager:
         self.blob = self.object.get_blob
 
     def upload(self, file_path):
+        """Uploads to a bucket from a local file path"""
         self.blob.upload_from_filename(absolute_path(file_path))
         return f'Uploading {self.object.blob_name}'
 
     def delete(self):
+        """Deletes a blob"""
         self.blob.delete()
         return f'{self.blob_name} Deleted'
 
     def download_file(self, download_to='downloads/'):
+        """Downloads a blob locally, default folder - downloads"""
         self.blob.download_to_filename(
             f'{download_to}{self.blob_name}')
         return f'Downloaded {self.blob_name}'
 
     @property
     def get_string(self):
+        """Saves bytes string of a blob to a variable"""
         return self.blob.download_as_string()
 
-
-b = BlobManager(credentials_path=CREDENTIALS_JSON, blob_path=BLOB_PATH,
-                blob_name=BLOB_NAME, bucket_name=BUCKET_NAME,
-                project_name=PROJECT_NAME)
-
-b.upload('temporary_data/archil.csv')
