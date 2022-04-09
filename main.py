e9c96dcae7d569870ff440ff9433b4c9dbc4140a
Archil Tsimakuidze <a.tsimakuridze16@gmail.com>
2022-04-09 14:05:14 +0400

    Production ready pre-final push

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ from helpers.path_formatter import absolute_path
 
 # Blob manager class parameters
 BUCKET_NAME = 'original-bucket'
-BLOB_NAME = 'nino.csv'
+BLOB_NAME = 'regionaldata.csv'
 BLOB_PATH = ''
 PROJECT_NAME = '3rd-month-project'
 CREDENTIALS_JSON = absolute_path('temporary_config/credentials.json')
 
 # Class template, uncomment for use
-b = BlobManager(credentials_path=CREDENTIALS_JSON, blob_path=BLOB_PATH,
-                blob_name=BLOB_NAME, bucket_name=BUCKET_NAME,
-                project_name=PROJECT_NAME)
+# blob_object = BlobManager(credentials_path=CREDENTIALS_JSON,
+#                           blob_path=BLOB_PATH,
+#                           blob_name=BLOB_NAME,
+#                           bucket_name=BUCKET_NAME,
+#                           project_name=PROJECT_NAME)
 
 
 def main():
-    b.upload('temporary_data/nino.csv')
+    # blob_object.upload('data/regionaldata.csv')
     return True
 
 
